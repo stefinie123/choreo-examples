0c02b3f0f1721c0bd98f1a414463d19e689cf483
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2023-05-10 09:49:25 +0530

    fix frontend

--- microservices/reading-list-front-end/src/main.tsx
+++ microservices/reading-list-front-end/src/main.tsx
@@ -15,16 +15,25 @@
 // under the License.
 
 
-import { AxiosResponse } from "axios";
-import { getReadingListInstance } from "./instance";
-import { Book } from "./types/book";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from ".";
+import "./App.css";
+import "./index.css";
+import { AuthProvider } from "@asgardeo/auth-react";
 
-export async function getBooks(accessToken: string) {
-  const headers = {
-    Authorization: `Bearer ${accessToken}`,
-  };
-  const response = await getReadingListInstance().get("/books", {
-    headers: headers,
-  });
-  return response as AxiosResponse<Book[]>;
-}
+const authConfig = {
+  signInRedirectURL: import.meta.env.VITE_REDIRECT_URL,
+  signOutRedirectURL: import.meta.env.VITE_REDIRECT_URL,
+  clientID: import.meta.env.VITE_ASGARDEO_CLIENT_ID,
+  baseUrl: import.meta.env.VITE_ASGARDEO_BASE_URL,
+  scope: ["openid", "profile"],
+};
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <AuthProvider config={authConfig}>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
